f8b868effe9bab3275e90ef67457c505b5361d67
Xen Land <xennetwork@hotmail.com>
2011-06-27 16:49:40 -0700

    Minor Fixes

---
+++ miningfarm.sql
@@ -0,0 +1,242 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 27, 2011 at 03:35 PM
+-- Server version: 5.1.54
+-- PHP Version: 5.3.5-1ubuntu7.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `miningfarm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accountBalance`
+--
+
+CREATE TABLE IF NOT EXISTS `accountBalance` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `userId` int(255) NOT NULL,
+  `balance` varchar(10) NOT NULL,
+  `payoutAddress` varchar(255) NOT NULL,
+  `threshhold` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userId` (`userId`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
+
+--
+-- Dumping data for table `accountBalance`
+--
+
+INSERT INTO `accountBalance` (`id`, `userId`, `balance`, `payoutAddress`, `threshhold`) VALUES
+(40, 43, '126.871875', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogPosts`
+--
+
+CREATE TABLE IF NOT EXISTS `blogPosts` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `timestamp` int(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `blogPosts`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `networkBlocks`
+--
+
+CREATE TABLE IF NOT EXISTS `networkBlocks` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `blockNumber` int(255) NOT NULL,
+  `timestamp` int(255) NOT NULL,
+  `txid` varchar(255) NOT NULL,
+  `confirms` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `networkBlocks`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pool_worker`
+--
+
+CREATE TABLE IF NOT EXISTS `pool_worker` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `associatedUserId` int(255) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `pool_worker`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shares`
+--
+
+CREATE TABLE IF NOT EXISTS `shares` (
+  `id` bigint(30) NOT NULL AUTO_INCREMENT,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `epochTimestamp` int(50) NOT NULL,
+  `rem_host` varchar(255) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `our_result` enum('Y','N') NOT NULL,
+  `upstream_result` enum('Y','N') DEFAULT NULL,
+  `reason` varchar(50) DEFAULT NULL,
+  `solution` varchar(257) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `shares`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shares_history`
+--
+
+CREATE TABLE IF NOT EXISTS `shares_history` (
+  `id` bigint(30) NOT NULL AUTO_INCREMENT,
+  `shareCounted` int(1) NOT NULL,
+  `blockNumber` int(255) NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `rem_host` varchar(255) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `our_result` enum('Y','N') NOT NULL,
+  `upstream_result` enum('Y','N') DEFAULT NULL,
+  `reason` varchar(50) DEFAULT NULL,
+  `solution` varchar(257) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `shares_history`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stats_poolMHashHistory`
+--
+
+CREATE TABLE IF NOT EXISTS `stats_poolMHashHistory` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `totalMhash` varchar(50) NOT NULL,
+  `averageMhash` varchar(20) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `stats_poolMHashHistory`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stats_userMHashHistory`
+--
+
+CREATE TABLE IF NOT EXISTS `stats_userMHashHistory` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `mhashes` varchar(20) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `stats_userMHashHistory`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `websiteSettings`
+--
+
+CREATE TABLE IF NOT EXISTS `websiteSettings` (
+  `header` varchar(255) NOT NULL,
+  `noreplyEmail` text NOT NULL,
+  `confirmEmailPrefix` text NOT NULL COMMENT 'The text or HTML written email that is sent for email confirmation',
+  `slogan` varchar(255) NOT NULL,
+  `browserTitle` varchar(255) NOT NULL,
+  `cashoutMinimum` varchar(5) NOT NULL COMMENT 'The minimum balance required before a user can cash out',
+  `serverFeePercentage` varchar(20) NOT NULL COMMENT 'Server fee in percents',
+  `tradeHillWorth` varchar(20) NOT NULL COMMENT 'Current worth of bitcoins',
+  `currencyData` varchar(20) NOT NULL COMMENT 'Three letter identifiers what the general default currency should be set too'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `websiteSettings`
+--
+
+INSERT INTO `websiteSettings` (`header`, `noreplyEmail`, `confirmEmailPrefix`, `slogan`, `browserTitle`, `cashoutMinimum`, `serverFeePercentage`, `tradeHillWorth`, `currencyData`) VALUES
+('Mining Pool v4', 'no-reply@yourdomain.com', 'Welcome you "Your pool name here" glad you are interested in our services, In order to activate your account you must click the link provided and you will be allowed immediate login access past this point. Thank you for you time.', 'IP:66.197.184.28 · PORT: 8341', 'Mining Pool', '1', '3', '15.3', 'tradehill-USD');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `websiteUsers`
+--
+
+CREATE TABLE IF NOT EXISTS `websiteUsers` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `isAdmin` int(1) NOT NULL,
+  `disabled` int(1) NOT NULL DEFAULT '0',
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `randomSecret` varchar(10) NOT NULL COMMENT 'Generated at login, this secret secures cookies when hashing',
+  `sessTimestamp` int(255) NOT NULL COMMENT 'Session timestamp for valid cookie checking',
+  `loggedIp` varchar(255) NOT NULL COMMENT 'Validating hashed cookies',
+  `email` varchar(255) NOT NULL,
+  `emailAuthorised` int(1) NOT NULL DEFAULT '0',
+  `emailAuthorisePin` varchar(64) NOT NULL,
+  `authPin` varchar(255) NOT NULL COMMENT 'A pin that must be supplied when changing details to various things',
+  `apiToken` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `websiteUsers`
+--
+
